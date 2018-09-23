4df7cc7609c1d1888e0e5084e94f51372a5a9c7d
Joonas Yliaho <joonas.yliaho@gmail.com>
2018-09-23 16:08:35 +0300

    finish operation query endpoint

--- src/controllers/operation.getOperation.ts
+++ src/controllers/operation.getOperation.ts
@@ -1,23 +1,27 @@
 import { RequestHandler } from 'express'
 import logger from '@/logger/logger'
-import { aql } from 'arangojs'
-import { getDb } from '@/database'
+import { queryOperationSection, queryOperation } from '@/models/operationModel'
 
 export const getOperation: RequestHandler = async (req, res, next) => {
   try {
     const { operationId, section } = req.body
 
-    const db = getDb()
+    if (operationId && Array.isArray(section)) {
+      // fetch operation data with filtered index and subindex
+      const [index, subIndex] = section
 
-    const operationQuery = await db.query(aql`
-      FOR op IN operations
-        FILTER op.internalId == ${operationId}
-        RETURN op
-    `)
+      const result = await queryOperationSection(operationId, index, subIndex)
 
-    const { _rev, _id, ...result } = await operationQuery.next()
+      res.status(200).send(result)
+    } else if (operationId) {
+      // fetch an operation as is
+      const result = await queryOperation(operationId)
 
-    res.status(200).send({ operation: result })
+      res.status(200).send(result)
+    } else {
+      // if no operationId was found in the body
+      res.status(400).send({ message: `Please provide operationId` })
+    }
   } catch (err) {
     next(err)
   }
--- src/models/operationModel.ts
+++ src/models/operationModel.ts
@@ -1,1 +1,63 @@
-import { Database } from 'arangojs'
+import { aql } from 'arangojs'
+import { getDb } from '@/database'
+
+export const queryOperation = async operationId => {
+  try {
+    const opQuery = await getDb().query(aql`
+      FOR op IN operations
+        FILTER op.internalId == ${operationId}
+        LIMIT 1
+        RETURN op
+    `)
+
+    const result = await opQuery.next()
+
+    return result
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export const queryOperationSection = async (
+  operationId: string,
+  index: number,
+  subIndex: number
+) => {
+  try {
+    const opSectionQuery = await getDb().query(aql`
+      FOR op IN operations
+        FILTER op.internalId == ${operationId}
+        LIMIT 1
+        LET section = (
+          FOR section IN op.data
+            FILTER section.index == ${index}
+            LIMIT 1
+            LET subheader = (
+              FOR sub IN section.subheaders
+                FILTER sub.index == ${subIndex}
+                LIMIT 1
+                RETURN sub
+            )
+            RETURN {
+              index: section.index,
+              title: section.title,
+              content: section.content,
+              labels: section.labels,
+              subheader: subheader
+            }
+        )
+        RETURN {
+          operation: op.operation,
+          area: op.area,
+          internalId: op.internalId,
+          section: section
+        }
+    `)
+
+    const result = await opSectionQuery.next()
+
+    return result
+  } catch (err) {
+    throw new Error(err)
+  }
+}
