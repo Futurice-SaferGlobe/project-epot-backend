44818d4b0d126f3c1adc9580f3e393e693bb1872
Joonas Yliaho <joonas.yliaho@gmail.com>
2018-09-23 15:09:09 +0300

    remove unused var

--- src/controllers/operation.getOperation.ts
+++ src/controllers/operation.getOperation.ts
@@ -3,33 +3,6 @@ import logger from '@/logger/logger'
 import { aql } from 'arangojs'
 import { getDb } from '@/database'
 
-const operations = [
-  {
-    operation: 'Unamid',
-    area: 'Darfur',
-    operationId: 'unamid',
-    data: [
-      {
-        index: 1,
-        title: 'Somekind of Title',
-        content: 'Section content for "Somekind of Title"',
-        subheaders: [
-          {
-            index: 1,
-            title: 'Conflict Prevention',
-            content: 'Subheader section content for "Conflict Prevention"'
-          },
-          {
-            index: 2,
-            title: 'Digitalents Helsinki',
-            content: 'Subheader section content for "Digitalents Helsinki"'
-          }
-        ]
-      }
-    ]
-  }
-]
-
 export const getOperation: RequestHandler = async (req, res, next) => {
   try {
     const { operationId, section } = req.body
